440f44d68163a1f9621b1707751e1191bb7bb3e7
dear_weronika <weronika.harlos@gmail.com>
2024-02-28 15:12:31 +0000

    adding

---
+++ src/components/HapticTestFailed.js
@@ -0,0 +1,10 @@
+export default function HapticTestFailed(params) {
+
+    return (
+        <div>
+            <h1>Soory but your device doesn't meet technial req for this test</h1>
+            <p>Thank you BYEEEEEE</p>
+
+        </div>
+    )
+};
---
+++ src/components/PostQuest.js
@@ -0,0 +1,9 @@
+export default function PostQuest(params) {
+    
+    return (
+        <div>
+            <h1>Post Questionnaire</h1>
+        </div>
+    )
+
+};
