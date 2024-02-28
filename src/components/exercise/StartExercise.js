08d4ff8484def94deca00a6d7be0ac98263ea727
dear_weronika <weronika.harlos@gmail.com>
2024-02-28 15:09:46 +0000

    affing survey

--- src/components/exercise/StartExercise.js
+++
@@ -1,9 +0,0 @@
-export default function StartExercise({handleClick}) {
-
-    return (
-        <div>
-            <p>Breathing Exercise Info</p>
-            <button onClick={handleClick}>Start</button>
-        </div>
-    )
-};
