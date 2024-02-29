9174d1d104b0af11d8a1350ca38fdc9ecb349a3a
dear_weronika <weronika.harlos@gmail.com>
2024-02-29 14:12:18 +0000

    added new input questions

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,44 +1,59 @@
-import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material"
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from "@mui/material"
 import { useEffect, useState } from "react"
 
-export default function Survey({handleSendData, handleClick}) {
+export default function Survey({ handleSendData, handleClick }) {
 
     const [data, setData] = useState({
-        gender: ""
-
+        gender: "",
+        question: ""
     })
+
     const handleChange = (e) => {
         const name = e.target.name
         const value = e.target.value
         setData(values => ({ ...values, [name]: value }))
     }
-    const handleSubmit = (e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault()
         handleSendData(data)
         handleClick()
     }
     useEffect(() => {
         console.log(data)
-    }, [data] )
+    }, [data])
     return (
         <div>
             <h5>Survery Final</h5>
-            <FormControl>
-                <FormLabel id="gender">
-                    Gender
-                </FormLabel>
-                <RadioGroup
-                    aria-labelledby="gender"
-                    name="gender"
-                    value={data.gender}
+            <Box sx={{ width: 400, p: 2 }}>
+                <h2>Question 1</h2>
+                <h3>human?</h3>
+                <FormControl>
+                    <FormLabel id="gender">
+                        Gender
+                    </FormLabel>
+                    <RadioGroup
+                        aria-labelledby="gender"
+                        name="gender"
+                        value={data.gender}
+                        onChange={handleChange}
+                    >
+                        <FormControlLabel value="female" control={<Radio />} label="Female" />
+                        <FormControlLabel value="male" control={<Radio />} label="Male" />
+                        <FormControlLabel value="other" control={<Radio />} label="Other" />
+                        <FormControlLabel value="prefer not to say" control={<Radio />} label="Prefer not to say" />
+                    </RadioGroup>
+                </FormControl>
+            </Box>
+            <Box sx={{ width: 400, p: 2 }}>
+                <h2>Question 2</h2>
+                <h3>Why do you prefer the version with or without haptic feedback?</h3>
+                <TextField
+                    name="question"
+                    value={data.question}
                     onChange={handleChange}
-                >
-                    <FormControlLabel value="female" control={<Radio />} label="Female" />
-                    <FormControlLabel value="male" control={<Radio />} label="Male" />
-                    <FormControlLabel value="other" control={<Radio />} label="Other" />
-                    <FormControlLabel value="prefer not to say" control={<Radio />} label="Prefer not to say" />
-                </RadioGroup>
-            </FormControl>
+                    label="Write something"
+                />
+            </Box>
             <button onClick={handleSubmit}>Click</button>
         </div>
     )
