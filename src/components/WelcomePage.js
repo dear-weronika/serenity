aea9768d74f13fa146feaf1692c8eab5f76980bc
dear_weronika <weronika.harlos@gmail.com>
2024-05-06 16:10:18 +0100

    added QR code and image

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -12,9 +12,11 @@ export default function WelcomePage({ handleClick }) {
                 Between exercises, we'll check-in on your mood. Once you've completed the exercises, there will be a survey for you to fill out. </p>
                 <p><strong>Reminder:</strong>
                 Your participation is voluntary, and you can opt out at any time.</p>
-                <p>To begin PRESS button below</p>
+                <p>To begin PRESS the button below</p>
                <p>Thanks again for your involvement!</p>
             <Button variant="contained" onClick={handleClick}>Start</Button>
+            <p>You need to complete this study on an android smartphone. If you are seeing this on your laptop and want to switch to your android device, you can scan below barcode or visit <a href="https://theserenity.netlify.app">https://theserenity.netlify.app</a></p>
+            <img src="qrcode_theserenity.netlify.app.png" style={{width:200}}></img>
             <p>Version 0.1.1</p>
         </div>
     )
