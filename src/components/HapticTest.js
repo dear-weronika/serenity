195253a34f188fe23d732fc60b4f5c8fba0818b3
dear_weronika <weronika.harlos@gmail.com>
2024-05-06 16:10:17 +0100

    styling buttons

--- src/components/HapticTest.js
+++ src/components/HapticTest.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from "@mui/material";
 
 export default function HapticTest({ handleYes, handleNo }) {
     const [state, setState] = useState()
@@ -10,17 +11,30 @@ export default function HapticTest({ handleYes, handleNo }) {
     }
     const validation = () => (
         <div>
-            <p>Did haptic work ?</p>
-            <button onClick={handleYes}>yes</button>
-            <button onClick={handleNo}>no</button>
+            <p>Did vibrations worked ?</p>
+            <div style={{ display: 'flex', gap: '10px' }}>
+                <Button variant="contained" size="small" onClick={handleYes}>yes</Button>
+                <Button variant="contained" size="small" onClick={handleNo}>no</Button>
+            </div>
         </div>
     )
     // navigator.vibrate([100, 300, 100, 30, 100, 30, 200, 30, 200, 30, 200, 30, 100, 30, 100, 30, 100]
 
     return (
         <div>
-            <p>Click on the button to check if haptic/vibration works !</p>
-            <button onClick={() => test()}>Haptic Test</button>
+            {/* <p>Click on the button to check if haptic/vibration works !</p> */}
+            <p>Before testing the vibration functionality:</p>
+            <ul>
+                <li>Ensure that your phone is an <strong>ANDROID Device</strong></li>
+                <br></br>
+                <li>Ensure your phone's sound mode is <strong>NOT SILENT</strong></li>
+                <br></br>
+                <li>Press the button a few times to initiate the vibration test</li>
+            </ul>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Button variant="contained" onClick={() => test()}>Check Vibration Now </Button>
+            </div>
+           
             {state}
 
         </div>
