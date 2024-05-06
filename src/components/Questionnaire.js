5d33f465c7e1dcf93c71acf0dd622a191e61a32f
dear_weronika <weronika.harlos@gmail.com>
2024-05-06 16:10:17 +0100

    change labels value

--- src/components/Questionnaire.js
+++ src/components/Questionnaire.js
@@ -1,46 +1,47 @@
 import { Box, Slider } from "@mui/material";
 import { useState } from "react";
+import MediaQuery from 'react-responsive'
 
 const marksOne = [
     {
         value: 1,
-        label: "Low Stress"
+        label: "Low"
     },
     {
         value: 5,
-        label: "Moderate Stress"
+        label: "Moderate"
     },
     {
         value: 10,
-        label: "Intense Stress"
+        label: "Intense"
     }
 ]
 const marksTwo = [
     {
         value: 1,
-        label: "Calm and peaceful"
+        label: "Calm"
     },
     {
         value: 5,
-        label: "Moderately relaxed"
+        label: "Relaxed"
     },
     {
         value: 10,
-        label: "Feeling anxious "
+        label: "Anxious "
     }
 ]
 const marksThree = [
     {
         value: 1,
-        label: "Anxious/Tense"
+        label: "Stressed"
     },
     {
         value: 5,
-        label: "Moderately Relaxed"
+        label: "Neutral"
     },
     {
         value: 10,
-        label: "Extremely Relaxed."
+        label: "Relaxed"
     }
 ]
 
@@ -65,12 +66,15 @@ export default function Questionnaire({ handleClick, version, handleSendData }) 
     }
 
     return (
+        
+        
         <Box>
             <h3>Questionnaire {version}</h3>
+            <MediaQuery minWidth={1224}>
             <Box sx={{ width: 400, p: 2 }}>
                 <h4>Question 1</h4>
-                <h5>How would you rate your current level of stress on a scale of 1 to 10?</h5>
-                <Slider
+                <h5>How would you rate your current stress level on a scale of 1 to 10?</h5>
+                <Slider sx={{width:400}}
                     name="questionOne"
                     marks={marksOne}
                     valueLabelDisplay="auto"
@@ -79,12 +83,13 @@ export default function Questionnaire({ handleClick, version, handleSendData }) 
                     shiftStep={1}
                     min={1}
                     max={10}
+                    className="slider-width100"
                 />
             </Box>
             <Box sx={{ width: 400, p: 2 }}>
                 <h4>Question 2</h4>
                 <h5>How would you describe your current state of mind</h5>
-                <Slider
+                <Slider sx={{width:400}}
                     name="questionTwo"
                     marks={marksTwo}
                     valueLabelDisplay="auto"
@@ -98,7 +103,7 @@ export default function Questionnaire({ handleClick, version, handleSendData }) 
             <Box sx={{ width: 400, p: 2 }}>
                 <h4>Question 3</h4>
                 <h5>How relaxed do you feel right now?</h5>
-                <Slider
+                <Slider sx={{width:400}}
                     name="questionThree"
                     marks={marksThree}
                     valueLabelDisplay="auto"
@@ -109,7 +114,54 @@ export default function Questionnaire({ handleClick, version, handleSendData }) 
                     max={10}
                 />
             </Box>
-            <button onClick={handleSubmit}>Click</button>
+            </MediaQuery>
+            <MediaQuery maxWidth={1224}>
+            <Box sx={{ width: 300, p: 2 }}>
+                <h4>Question 1</h4>
+                <h5>How would you rate your current level of stress on a scale of 1 to 10?</h5>
+                <Slider sx={{width:250,fontSize:12}}
+                    name="questionOne"
+                    marks={marksOne}
+                    valueLabelDisplay="auto"
+                    value={data.questionOne}
+                    onChange={handleChange}
+                    shiftStep={1}
+                    min={1}
+                    max={10}
+                />
+            </Box>
+            <Box sx={{ width: 300, p: 2 }}>
+                <h4>Question 2</h4>
+                <h5>How would you describe your current state of mind</h5>
+                <Slider sx={{width:250,fontSize:12}}
+                    name="questionTwo"
+                    marks={marksTwo}
+                    valueLabelDisplay="auto"
+                    value={data.questionTwo}
+                    onChange={handleChange}
+                    shiftStep={1}
+                    min={1}
+                    max={10}
+                />
+            </Box>
+            <Box sx={{ width: 300, p: 2 }}>
+                <h4>Question 3</h4>
+                <h5>How relaxed do you feel right now?</h5>
+                <Slider sx={{width:250,fontSize:12}}
+                    name="questionThree"
+                    marks={marksThree}
+                    valueLabelDisplay="auto"
+                    value={data.questionThree}
+                    onChange={handleChange}
+                    shiftStep={1}
+                    min={1}
+                    max={10}
+                />
+            </Box>
+            </MediaQuery>
+            <button onClick={handleSubmit}>Click</button> 
         </Box>
+      
+        
     )
 };
