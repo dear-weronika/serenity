98a5a72576daae987f791c0875344d958e3bd8cb
dear_weronika <weronika.harlos@gmail.com>
2024-05-06 16:10:17 +0100

    info

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,10 +1,10 @@
 export default function WelcomePage({ handleClick }) {
     return (
         <div>
-            <h1>WelcomeB 1 </h1>
-            <p>Ethics Approval</p>
+            <h1>Welcome to Serenity+</h1>
+            <p>Ethics Approval??// Disclaimer</p>
             <p> Make you sure your vibration settings are on "especially your phone is not on silence mode ".</p>
-            <p>Short description of the app</p>
+            <p>Short description</p>
             <button onClick={handleClick}>Start</button>
 
         </div>
